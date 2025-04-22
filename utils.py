fdab795116ffd3aa22b86dbaabea04416293a4b8
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-04-22 14:35:54 +0530

    Utils File

--- utils.py
+++ utils.py
@@ -66,6 +66,33 @@ class ReplayBuffer:
                     i = random.randint(0, self.max_size - 1)
                     to_return.append(self.data[i].clone())
                     self.data[i] = element
+                # returns an older generated image
                 else:
                     to_return.append(element)
         return Variable(torch.cat(to_return))
+
+class LambdaLR:
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert(
+            n_epochs - decay_start_epoch
+        ) > 0, "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        # Checks whether the current epoch has exceeded the decay epoch
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
+        
+
+def initialize_weights(m):
+
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, "bias") and m.bias is not None:
+            nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find("BatchNorm2d") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
